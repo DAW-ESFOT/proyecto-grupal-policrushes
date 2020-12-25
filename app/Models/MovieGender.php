855a1a78a5000294da8a30d89c4188cd5a3e4f14
Ronny Cajas <ronny.cajas@epn.edu.ec>
2020-12-25 17:30:18 -0500

    corrige nombres de modelos

--- app/Models/MovieGender.php
+++ app/Models/MovieGender.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class MusicGenders extends Model
+class MovieGender extends Model
 {
     use HasFactory;
     protected $fillable = ['name'];
