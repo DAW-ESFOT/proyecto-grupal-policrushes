d07fa1877d8a34f09833ccfe12e2dc729670fc6d
Ronny Cajas <ronny.cajas@epn.edu.ec>
2020-12-25 17:56:43 -0500

    user controller, api endpoints

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Models\User;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//business
+Route::get('users', 'UserController@index'); 
+Route::get('users/{article}', 'UserController@show'); 
+Route::post('users', 'UserController@store');
+Route::put('users/{article}', 'UserController@update'); 
+Route::delete('users/{article}', 'UserController@delete');
