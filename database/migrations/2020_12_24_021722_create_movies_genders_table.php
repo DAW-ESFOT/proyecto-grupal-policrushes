e565d487f379e95f8a01d2909349635b7bb1444f
Ronny Cajas <ronny.cajas@epn.edu.ec>
2020-12-25 23:18:52 -0500

    movies and music seeding, quick fix on movies migration

--- database/migrations/2020_12_24_021722_create_movies_genders_table.php
+++ database/migrations/2020_12_24_021722_create_movies_genders_table.php
@@ -13,7 +13,7 @@ class CreateMoviesGendersTable extends Migration
      */
     public function up()
     {
-        Schema::create('movies_genders', function (Blueprint $table) {
+        Schema::create('movie_genders', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
             $table->text('name')->nullable();
@@ -27,6 +27,6 @@ class CreateMoviesGendersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('movies_genders');
+        Schema::dropIfExists('movie_genders');
     }
 }
