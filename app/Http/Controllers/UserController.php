65f947d7ec7d41b8a722c04f4b691d56bdc7ede3
Stefani Troya <josselyn.troya@epn.edu.ec>
2021-01-23 19:21:05 -0500

    Imagenes para user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,9 +8,12 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use JWTAuth;
 use Tymon\JWTAuth\Exceptions\JWTException;
+use Illuminate\Support\Facades\Storage;
+
 
 class UserController extends Controller
 {
+
     public function authenticate(Request $request)
     {
         $credentials = $request->only('email', 'password');
@@ -24,17 +27,28 @@ class UserController extends Controller
         return response()->json(compact('token'));
     }
 
+
+    public function image(User $user)
+    {
+        return response()->download(public_path(Storage::url($user->image)),
+            $user->name);
+    }
+
+
     public function register(Request $request)
     {
         $validator = Validator::make($request->all(), [
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
+            'image' => 'required|image|dimensions:min_width=200,min_height=200',
         ]);
 
         if ($validator->fails()) {
             return response()->json($validator->errors()->toJson(), 400);
         }
+
+        $path = $request->image->store('public/images');
 
         $user = User::create([
             'name' => $request->get('name'),
@@ -47,8 +61,14 @@ class UserController extends Controller
         return response()->json(compact('user', 'token'), 201);
     }
 
-    public
-    function getAuthenticatedUser()
+
+
+    public function store(Request $request)
+    {
+
+    }
+
+    public function getAuthenticatedUser()
     {
         try {
             if (!$user = JWTAuth::parseToken()->authenticate()) {
@@ -63,4 +83,10 @@ class UserController extends Controller
         }
         return response()->json(compact('user'));
     }
+
+    public function show(User $user)
+    {
+        return response()->json(new UserResource($user), 200);
+    }
+
 }
--- database/seeders/UserTableSeder.php
+++ database/seeders/UserTableSeder.php
@@ -17,15 +17,20 @@ class UserTableSeder extends Seeder
      */
     public function run()
     {
-        // Vaciar la tabla 
+        // Vaciar la tabla
         User::truncate();
         $faker = \Faker\Factory::create();
+
+        $image_name = $faker->image('public/storage/images', 400, 300, null, false);
+
         // Crear la misma clave para todos los usuarios
         // conviene hacerlo antes del for para que el seeder // no se vuelva lento.
         $password = Hash::make('123123');
         User::create([
-            'name' => 'Administrador', 
-            'email' => 'admin@prueba.com', 
+            'name' => 'Administrador',
+            'email' => 'admin@prueba.com',
+            'image' => 'users/' . $image_name,
+
             'password' => $password,
         ]);
         // Generar algunos usuarios para nuestra aplicacion for ($i = 0; $i < 10; $i++) {
@@ -33,9 +38,10 @@ class UserTableSeder extends Seeder
             $user = User::create([
                 'name' => $faker->name,
                 'email' => $faker->email,
+                'image'=> $image_name,
                 'password' => $password,
             ]);
-            
+
             $user->music()->saveMany(
                 $faker->randomElements(
                     array(
@@ -44,7 +50,7 @@ class UserTableSeder extends Seeder
                         MusicGender::find(3),
                         MusicGender::find(4),
                     ),$faker->numberBetween(1, 3), false,
-                    array(  
+                    array(
                         MovieGender::find(1),
                         MovieGender::find(2),
                         MovieGender::find(3),
@@ -53,14 +59,14 @@ class UserTableSeder extends Seeder
             );
     /*
             $user->movie()->saveMany(
-                array(  
+                array(
                         MovieGender::find(1),
                         MovieGender::find(2),
                         MovieGender::find(3),
                         MovieGender::find(4),
                     ),$faker->numberBetween(1, 3), false)
                 $faker->randomElements(
-                    
+
             );*/
         }
     }
